2dc27ec2bea67e82866f504f248ad15183d0857c
João Pedro Godoy <128510375+joaopgodoy@users.noreply.github.com>
2024-07-03 11:37:40 -0300

    new process_files.py added new adjusts in extract_make_dataframe.py
    Co-authored-by: Caue Paiva <caue-paiva@users.noreply.github.com>

--- programas/extract_make_dataframe.py
+++ programas/extract_make_dataframe.py
@@ -1,15 +1,20 @@
 from link_extractor import extrair_links
 from AbstractScrapper import download_and_extract_zipfile
-import requests, zipfile, io
+import requests, zipfile, io, os
+from AbstractScrapper import EXTRACTED_FILES_DIR
 
+FILES_FOLDER_PATH = os.path.join(os.getcwd(), EXTRACTED_FILES_DIR) 
 
 if __name__ == "__main__":
     
     url = "https://www.gov.br/inep/pt-br/areas-de-atuacao/pesquisas-estatisticas-e-indicadores/censo-da-educacao-superior/resultados"
     links_encontrados = extrair_links(url)
+    
+    if not os.path.isdir(FILES_FOLDER_PATH):
+        os.mkdir(FILES_FOLDER_PATH)
+
     for link in links_encontrados:
         print(link)
-
         r = requests.get(link, verify=False)
         z = zipfile.ZipFile(io.BytesIO(r.content))
         z.extractall("/home/joao/Desktop/IARA_Intelli.gente/IARA_Inteli.gente/programas/extracted_files")
