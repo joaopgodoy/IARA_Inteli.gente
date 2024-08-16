6e35069176f7e82d90929116ca30939a5213711c
João Pedro Godoy <128510375+joaopgodoy@users.noreply.github.com>
2024-08-15 22:42:15 -0300

    NEW: TaxaDistorcaoIdadeSerieScrapper_test working from 2022 to 2016, creating dataframe correctly AbstractScrapper alterations to help coding the new addition YearDataPoint added to the folder

--- programas/taxa_distorcao_idade_serie/AbstractScrapper.py
+++ programas/taxa_distorcao_idade_serie/AbstractScrapper.py
@@ -1,91 +1,140 @@
-import os
-import requests
-import zipfile
-from DataEnums import BaseFileType
+from abc import ABC,abstractmethod
 import pandas as pd
-import io
+import os , requests , zipfile
+from DataEnums import BaseFileType
+from YearDataPoint import YearDataPoint
 
 
-# Suprimir os avisos de solicitações HTTPS não verificadas
 
-EXTRACTED_FILES_DIR = "extracted_files"
 
-def download_and_extract_zipfile(file_url: str) -> str:
-    """
-    Dado um URL para baixar um arquivo zip, baixa esse arquivo zip e extrai seu conteúdo,
-    retornando o caminho para o arquivo de dados extraído.
-    Args:
-       file_url (str): URL para baixar o arquivo .zip
-    """
-    # Caso o diretório para guardar os arquivos extraídos não exista, vamos criá-lo
-    if not os.path.exists(EXTRACTED_FILES_DIR):
-        os.makedirs(EXTRACTED_FILES_DIR)
+
+class AbstractScrapper(ABC):
+
+   """
+   Essa classe fornece a interface utilizada pelas classes de webscrapping,que podem receber ou não informações sobre qual dado extrair (os parâmetros de INIT
+   não são fixos e dependem de cada classe) e realizam a extração dos dado desses sites, retornando uma lista de objetos YearDataPoint. Algumas subclasses podem necessitar
+   primeiro baixar os arquivos como zip e depois extrair-los, para isso métodos gerais sobre isso são implementados nessa classe abstrata.
+
+   """
+
+   DOWNLOADED_FILES_DIR:str = "tempfiles" #diretório temporário para guardar os arquivos .zip e de dados extraidos
+   DOWNLOADED_FILES_PATH = os.path.join(os.getcwd(),DOWNLOADED_FILES_DIR)
+
+
+   @abstractmethod
+   def extract_database()->list[YearDataPoint]:
+      """
+      Extrai os dados daquele site e retorna uma lista de objetos YearDataPoint, em que cada objeto tem um dataframe e o ano
+      dos dados contidos nese dataframe    
+      
+      Return:
+         list[YearDataPoint]: lista de objetos com os dataframes dos dados e os anos a quais eles se referem   
+      """
+      
+      pass
+
+   def _download_and_extract_zipfile(self, file_url:str)->str:
+      """
+      Dado um URL para baixar um arquivo zip das bases oficiais, baixa esse arquivo zip e extrai seu conteúdo,
+      retornando o caminho para o arquivo de dados que extraido. Esse método é implementado na classe mãe abstrata, pois ele é genérico para a maioria
+      dos scrappers de diferentes bases
+
+      Args:
+         file_url (str): url para baixar o arquivo .zip das bases do IBGE
+      """
+     
+      #caso o diretório para guardar os arquivos extraidos não exista, vamos criar ele
+      if not os.path.exists(self.DOWNLOADED_FILES_DIR):
+         os.makedirs(self.DOWNLOADED_FILES_DIR)
+
+      #baixando o arquivo zip
+      response = requests.get(file_url) #request get para o link do arquivo zip 
+      if response.status_code == 200: #request com sucesso
+         zip_file_name =  "zipfile.zip"
+         zip_file_path = os.path.join(self.DOWNLOADED_FILES_DIR, zip_file_name)
     
-    # Baixando o arquivo zip
+         with open(zip_file_path, "wb") as f:
+            f.write(response.content) #escreve o arquivo zip no diretório de dados temporários
+      else:
+         raise RuntimeError("Falhou em baixar o arquivo .zip, status code da resposta:", response.status_code)
 
-    response = requests.get(file_url, verify=False)
-    if response.status_code == 200:  # Request com sucesso
-        zip_file_name = "zipfile.zip"
-        zip_file_path = os.path.join(EXTRACTED_FILES_DIR, zip_file_name)
-        
-        with open(zip_file_path, "wb") as f:
-            f.write(response.content)  # Escreve o arquivo zip no diretório de dados temporários
-    else:
-        raise RuntimeError(f"Falhou em baixar o arquivo .zip, status code da resposta: {response.status_code}")
-    
-    # Extraindo o arquivo zip
-    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
-        zip_ref.extractall(EXTRACTED_FILES_DIR)
-    
-    # No diretório de arquivos baixados e extraídos, vamos ter o .zip e o arquivo de dados extraído
-    extracted_files = os.listdir(EXTRACTED_FILES_DIR)
-    
-    data_file_name = ""
-    for file in extracted_files:
-        if ".zip" not in file:  # Acha o arquivo de dados, que é o único sem ser .zip
+      #extraindo o arquivo zip
+      with zipfile.ZipFile(os.path.join(self.DOWNLOADED_FILES_DIR, zip_file_name), "r") as zip_ref:
+            zip_ref.extractall(self.DOWNLOADED_FILES_DIR)
+
+      #no diretório de arquivos baixados e extraidos, vamos ter o .zip e o arquivo de dados extraido
+      extracted_files:list[str] = os.listdir(self.DOWNLOADED_FILES_DIR)
+      
+      data_file_name:str = ""
+      for file in extracted_files:
+         if ".zip" not in file: #acha o arquivo de dados, que é o único sem ser .zip
             data_file_name = file
             break
-    
-    if not extracted_files or not data_file_name:  # Nenhum arquivo foi extraído ou nenhum arquivo de dados foi encontrado
-        raise RuntimeError("Extração do arquivo zip no diretório temporário falhou")
-    
-    return os.path.join(EXTRACTED_FILES_DIR, data_file_name)  # Retorna o caminho para o arquivo extraído
 
+      if not extracted_files or not data_file_name: #nenhum arquivo foi extraido ou nenhum arquivo de dados foi encontrado
+         raise RuntimeError("Extração do arquivo zip num diretório temporário falhou")
+      
+      return os.path.join(self.DOWNLOADED_FILES_DIR, data_file_name) #retorna o caminho para o arquivo extraido
+   
+   def _dataframe_from_link(self, file_url:str, file_type: BaseFileType, zipfile: bool = True)->pd.DataFrame:
+      """
+      Dado um link para um arquivo , se ele for zip primeiro extrai e dps carrega o arquivo tabular extraido, caso não seja apenas usas as 
+      funções do pandas para carregar essa arquivo em um Dataframe
 
-def dataframe_from_link(file_url:str, file_type: BaseFileType, zipfile: bool = True)->pd.DataFrame:
-   """
-   Dado um link para um arquivo , se ele for zip primeiro extrai e dps carrega o arquivo tabular extraido, caso não seja apenas usas as 
-   funções do pandas para carregar essa arquivo em um Dataframe
-   Args:
-      file_url (str): url para baixa o arquivo
-      file_type (BaseFileType): tipo de arquivo a ser baixado
-      zipfile (bool): diz se o link é pra um arquivo zip ou não
-   Return:
-      (Dataframe): um df da base extraida
-   """
-   
-   if zipfile: #link  é pra um arquivo zip, vamos extrair ele primeiro
-      file_path:str = download_and_extract_zipfile(file_url) #chama o método da mesma classe de extrair o zipfile
-   else:
-      file_path:str = file_url  #link n é pra um arquivo zip, o argumento pode ser passado para o pandas direto
-   df:pd.DataFrame = None
-   try:
-      match (file_type): #match case no tipo de dado
-         case BaseFileType.EXCEL:
-            df = pd.read_excel(file_path)
-         case BaseFileType.ODS:
-            pass
-         case BaseFileType.CSV:
-            df = pd.read_csv(file_path)
-   except Exception as e:
-         raise RuntimeError(f"Não foi possível extrair o dataframe, tem certeza que o arquivo extraido do site não  é zip? Msg de erro? {e}")
-   
-   """
-   TODO
-   Colocar os casos para os outros tipos de arquivos, caso seja possível extrair dataframes deles
-   """
-   if df is None: #não foi possível extrair o DF
-      raise RuntimeError("não foi possível criar um dataframe a partir do link")
-   
-   return df
-  
+      Args:
+         file_url (str): url para baixa o arquivo
+         file_type (BaseFileType): tipo de arquivo a ser baixado
+         zipfile (bool): diz se o link é pra um arquivo zip ou não
+
+      Return:
+         (Dataframe): um df da base extraida
+      """
+      
+      if zipfile: #link  é pra um arquivo zip, vamos extrair ele primeiro
+         file_path:str = self._download_and_extract_zipfile(file_url) #chama o método da mesma classe de extrair o zipfile
+      else:
+         file_path:str = file_url  #link n é pra um arquivo zip, o argumento pode ser passado para o pandas direto
+
+      df:pd.DataFrame = None
+
+      try:
+         match (file_type): #match case no tipo de dado
+            case BaseFileType.EXCEL:
+               df = pd.read_excel(file_path)
+            case BaseFileType.ODS:
+               pass
+            case BaseFileType.CSV:
+               df = pd.read_csv(file_path)
+      except Exception as e:
+            raise RuntimeError(f"Não foi possível extrair o dataframe, tem certeza que o arquivo extraido do site não  é zip? Msg de erro? {e}")
+      
+      """
+      TODO
+      Colocar os casos para os outros tipos de arquivos, caso seja possível extrair dataframes deles
+      """
+
+      if df is None: #não foi possível extrair o DF
+         raise RuntimeError("não foi possível criar um dataframe a partir do link")
+      
+      return df
+     
+   def _delete_download_files_dir(self)->bool:
+        files:list[str] = os.listdir(self.DOWNLOADED_FILES_PATH)
+
+        for file in files:
+            try:
+               os.remove(os.path.join(self.DOWNLOADED_FILES_PATH,file))
+            except Exception as e:
+               print(f"falha ao deletar um arquivo extraído. Erro: {e}")
+        
+        try:
+            os.rmdir(self.DOWNLOADED_FILES_PATH)
+        except Exception as e:
+               print(f"falha ao deletar diretório do arquivo extraído. Erro: {e}")
+
+   def _create_downloaded_files_dir(self)->str:
+        dir_path: str = os.path.join(os.getcwd(),self.DOWNLOADED_FILES_DIR)
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+        
+        return dir_path
---
+++ programas/taxa_distorcao_idade_serie/TaxaDistorcaoIdadeSerieScrapper_teste.py
@@ -0,0 +1,210 @@
+import os
+import re
+import time
+import requests
+import zipfile
+import io
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from YearDataPoint import YearDataPoint
+from AbstractScrapper import AbstractScrapper
+
+class UnifiedScrapper(AbstractScrapper):
+
+    def __init__(self, url: str, regex_pattern_older: str, regex_pattern_newer: str):
+        self.url = url
+        self.regex_pattern_older = regex_pattern_older
+        self.regex_pattern_newer = regex_pattern_newer
+        self.files_folder_path = self._create_downloaded_files_dir()
+        print(f"Initialized UnifiedScrapper with URL: {url}")
+        print(f"Files will be saved in: {self.files_folder_path}")
+
+    def extrair_links(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get(self.url)
+
+        # Esperar um pouco para garantir que a página carregue completamente
+        driver.implicitly_wait(10)
+
+        # Fechar o pop-up inicial clicando no meio da tela
+        self.fechar_popup_inicial(driver)
+
+        # Processar os anos desejados
+        anos = range(2006, 2022+1)
+        self.clicar_todos_os_anos(driver, anos)
+
+        # Obter o conteúdo da página renderizada após clicar em todos os anos
+        html_content = driver.page_source
+        driver.quit()
+
+        # Padrões regex para encontrar os links específicos
+        pattern_older = re.compile(self.regex_pattern_older)
+        pattern_newer = re.compile(self.regex_pattern_newer)
+
+        # Encontra os links usando ambos os padrões
+        links_older = pattern_older.findall(html_content)
+        links_newer = pattern_newer.findall(html_content)
+
+        # Combina ambos os tipos de links
+        links = links_older + links_newer
+        print(f"Links encontrados: {links}")
+
+        return links
+
+    def fechar_popup_inicial(self, driver):
+        try:
+            driver.implicitly_wait(5)
+            window_size = driver.get_window_size()
+            width = window_size['width']
+            height = window_size['height']
+            center_x = width / 2
+            center_y = height / 2
+            actions = ActionChains(driver)
+            actions.move_by_offset(center_x, center_y).click().perform()
+            print("Fechou o pop-up inicial clicando no meio da tela.")
+        except Exception as e:
+            print(f"Não foi possível fechar o pop-up inicial clicando no meio da tela: {e}")
+
+    def clicar_na_seta(self, driver, direcao):
+        if direcao == "esquerda":
+            selector = "#content-core > div.govbr-tabs.swiper-container-initialized.swiper-container-horizontal.swiper-container-free-mode > div.button-prev > span"
+        elif direcao == "direita":
+            selector = "#content-core > div.govbr-tabs.swiper-container-initialized.swiper-container-horizontal.swiper-container-free-mode > div.button-next > span"
+
+        try:
+            seta_element = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, selector))
+            )
+            seta_element.click()
+            print(f"Clicou na seta {direcao} para mostrar anos anteriores.")
+        except Exception as e:
+            print(f"Não foi possível clicar na seta {direcao}: {e}")
+
+    def clicar_todos_os_anos(self, driver, anos):
+        for i, ano in enumerate(anos):
+            while True:
+                try:
+                    print(f"Processando o ano {ano}...")
+                    ano_element = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.LINK_TEXT, str(ano)))
+                    )
+                    driver.execute_script("arguments[0].scrollIntoView();", ano_element)
+                    driver.execute_script("arguments[0].click();", ano_element)
+                    time.sleep(3)
+                    break
+                except Exception as e:
+                    print(f"Erro ao processar o ano {ano}: {e}")
+                    if i > 0 and ano > anos[i - 1]:
+                        print(f"Tentando clicar na seta para a esquerda para mostrar anos mais recentes...")
+                        self.clicar_na_seta(driver, "esquerda")
+                    else:
+                        print(f"Tentando clicar na seta para a direita para mostrar anos mais antigos...")
+                        self.clicar_na_seta(driver, "direita")
+                    time.sleep(2)
+
+    def extract_database(self):
+        year_data_points = []
+
+        # Extração dos links das páginas
+        links = self.extrair_links()
+
+        if not os.path.isdir(self.files_folder_path):
+            os.mkdir(self.files_folder_path)
+
+        for link in links:
+            print(f"Baixando e extraindo: {link}")
+            r = requests.get(link, verify=False)
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            extract_path = os.path.join(self.files_folder_path, self.extract_year_from_link(link))
+            os.makedirs(extract_path, exist_ok=True)
+            z.extractall(extract_path)
+
+            year_data_point = self.data_dir_process(extract_path)
+            if year_data_point:
+                year_data_points.append(year_data_point)
+                print(f"Processamento concluído na pasta {extract_path} com sucesso.")
+            else:
+                print(f"Processamento falhou na pasta {extract_path}")
+
+        print(f"Total de YearDataPoints processados: {len(year_data_points)}")
+        return year_data_points
+
+    def extract_year_from_link(self, link: str) -> str:
+        match = re.search(r'\d{4}', link)
+        return match.group(0) if match else 'unknown_year'
+
+    def data_dir_process(self, folder_path: str) -> YearDataPoint:
+        print(f"Entrando na pasta: {folder_path}")
+        subfolders = [f.path for f in os.scandir(folder_path) if f.is_dir()]
+        print(f"Subpastas encontradas: {subfolders}")
+
+        for subfolder in subfolders:
+            files_list = os.listdir(subfolder)
+            print(f"Arquivos encontrados na subpasta {subfolder}: {files_list}")
+            for file in files_list:
+                if file.endswith(".xlsx") and ("TDI" in file or "tdi" in file):
+                    file_correct_path = os.path.join(subfolder, file)
+                    print(f"Processando arquivo: {file_correct_path}")
+                    df = self.process_df(file_correct_path)
+                    if df is not None:
+                        year = self.extract_year_from_path(folder_path)
+                        if year:
+                            print(f"Criando YearDataPoint para o ano {year}")
+                            return YearDataPoint(df=df, data_year=year)
+                        else:
+                            print(f"Falha ao extrair o ano do caminho: {folder_path}")
+                    else:
+                        print(f"Processamento falhou para o arquivo: {file_correct_path}")
+
+        print(f"Não foram encontrados arquivos .xlsx relevantes em {folder_path}")
+        return None
+
+    def process_df(self, xlsx_file_path: str) -> pd.DataFrame:
+        try:
+            df = pd.read_excel(xlsx_file_path, header=6, skiprows=[7, 8])
+            print(f"Colunas disponíveis no arquivo {xlsx_file_path}: {df.columns.tolist()}")
+
+            municipality_col = 'Unnamed: 4'
+            total_col = 'Total'
+            location_col = 'Unnamed: 5'
+            admin_dependency_col = 'Unnamed: 6'
+
+            if municipality_col not in df.columns or total_col not in df.columns or location_col not in df.columns or admin_dependency_col not in df.columns:
+                raise ValueError(f"Colunas necessárias ('{municipality_col}', '{total_col}', '{location_col}', '{admin_dependency_col}') não encontradas no arquivo {xlsx_file_path}")
+
+            df_filtered = df[(df[location_col] == 'Total') & (df[admin_dependency_col] == 'Total')]
+            filtered_df = df_filtered[[municipality_col, total_col]]
+            print(f"Dados filtrados:\n{filtered_df.head()}")  # Exibe uma amostra dos dados filtrados
+
+            return filtered_df
+        except Exception as e:
+            print(f"Erro ao processar o DataFrame: {e}")
+            return None
+
+    def extract_year_from_path(self, path: str) -> int:
+        print(f"Extraindo ano do caminho: {path}")
+        ano_match = re.search(r'\d{4}', path)
+        if ano_match:
+            year = int(ano_match.group(0))
+            print(f"Ano extraído: {year}")
+            return year
+        else:
+            print("Falha ao extrair o ano.")
+            return None
+
+
+if __name__ == "__main__":
+    url = "https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/indicadores-educacionais/taxas-de-distorcao-idade-serie"
+    regex_pattern_older = r'https://download\.inep\.gov\.br/informacoes_estatisticas/indicadores_educacionais/\d{4}/TDI_\d{4}_MUNICIPIOS.zip'
+    regex_pattern_newer = r'https://download\.inep\.gov\.br/informacoes_estatisticas/indicadores_educacionais/\d{4}/tdi_municipios_\d{4}.zip'
+
+    scrapper = UnifiedScrapper(url, regex_pattern_older, regex_pattern_newer)
+    year_data_points = scrapper.extract_database()
+
+    for data_point in year_data_points:
+        print(f"Ano: {data_point.data_year}, DataFrame Shape: {data_point.df.shape}")
