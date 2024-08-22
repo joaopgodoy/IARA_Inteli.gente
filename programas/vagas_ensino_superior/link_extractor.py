0187659d1a6f0b5b0b4b23685b345d9d2e8a33af
“Caue <cauepaivalira@outlook.com>
2024-08-22 15:58:23 -0300

    erro no código de baixar zips, codigo de baixa zips com selenium adicionados

--- programas/vagas_ensino_superior/link_extractor.py
+++ programas/vagas_ensino_superior/link_extractor.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -105,8 +104,9 @@ def extrair_links(url):
 # FUNÇÃO NOVA E BEM MAIS SIMPLES QUE EXTRAI DIRETO DO HTML DA PÁGINA 
 def extrair_links_simples(url, regex_pattern):
     # Faz a requisição HTTP para obter o conteúdo da página
-    response = requests.get(url)
-    response.raise_for_status()  # Garante que a requisição foi bem-sucedida
+    print(url)
+    response = requests.get(url) 
+    #response.raise_for_status()  # Garante que a requisição foi bem-sucedida
     
     # Obtém o conteúdo da página como texto
     html = response.text
