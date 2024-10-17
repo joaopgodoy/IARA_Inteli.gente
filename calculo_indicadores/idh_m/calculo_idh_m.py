814aab1106c1e08121429e0cc23ac8c10ee90e90
João Pedro Godoy <128510375+joaopgodoy@users.noreply.github.com>
2024-10-17 10:12:20 -0300

    CalculadoraIndicador para idh_m pronta e funcional

---
+++ calculo_indicadores/idh_m/calculo_idh_m.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from math import log
+from CalculadoraIndicador import CalculadoraIndicador
+
+def formula(valor, min, max):
+    res = ((log(valor) - log(min)) / (log(max) - log(min)))
+    return res
+
+def transforma_dataframe_idh_m(df):
+    # Carregar o arquivo CSV que está na mesma pasta do script
+
+    # Calcular o valor mínimo e máximo para cada ano
+    min_max_por_ano = df.groupby('ano')['valor'].agg(['min', 'max']).reset_index()
+
+    # Mesclar os valores mínimos e máximos no DataFrame original
+    df = df.merge(min_max_por_ano, on='ano', how='left')
+
+    # Aplicar a fórmula usando os mínimos e máximos específicos de cada ano
+    df['valor_modificado'] = df.apply(lambda row: formula(row['valor'], row['min'], row['max']), axis=1)
+
+    # Remover as colunas auxiliares 'min' e 'max' antes de salvar
+    df = df.drop(columns=['min', 'max'])
+
+    # Salvar o DataFrame modificado em um novo arquivo CSV
+    df.to_csv('idh_m_transformado.csv', index=False)
+
+calcula_idh = CalculadoraIndicador("idh_m",1,transforma_dataframe_idh_m,None)
+
+df = pd.read_csv('idh_m.csv')
+
+calcula_idh.funcao_calcula_valor(df)
+
