f9218aa68c0f506fd05ac1adb1dbc9bf917d27c9
João Pedro Godoy <128510375+joaopgodoy@users.noreply.github.com>
2025-02-06 12:36:37 -0300

    adição indicadores 3139, 3145, 4057 e 4058 (mudanças no dimensions.json e nos .py de econ e sociodem)

---
+++ indicadores/econ/3139.py
@@ -0,0 +1,12 @@
+from . import *
+
+def equation(row):
+    try:
+        return 100 * (row['Domicílios particulares permanentes urbanos com Existência de arborização de vias públicas'] / row['Total_Domicílios'])
+    except ZeroDivisionError:
+	    return 0
+    
+class EC_3139(processor):
+
+    def __init__(self, data_list):
+        super().__init__(data_list['3139'], score=equation)
